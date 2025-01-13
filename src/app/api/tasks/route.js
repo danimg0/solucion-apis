6a87337be946d2563b957cda9b280bc690fc0b46
Dani <daniel.martos@hotmail.com>
2025-01-13 14:47:19 +0100

    asd

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,14 +1,22 @@
-import Tasks from "@/app/tasks/page"
+// import Tasks from "@/app/tasks/page"
+import { createClient } from '@supabase/supabase-js'
 
-export let tareas = [
-    {id: 1, title: "preparar la comida", completed: true},
-    {id: 2, title: "hacer la colada", completed: false}
-]
+// export let tareas = [
+//     {id: 1, title: "preparar la comida", completed: true},
+//     {id: 2, title: "hacer la colada", completed: false}
+// ]
+
+
+const supabaseUrl = 'https://rmpqbxpembolxycjtadm.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtcHFieHBlbWJvbHh5Y2p0YWRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3NTg1MzYsImV4cCI6MjA1MjMzNDUzNn0.zoNvI5PMX-YK8DFsIe8K0mFU-e6WCvn_OJOJcUkXVm4"
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export async function GET(request){
    
    const {searchParams} = new URL(request.url)
    const filter = searchParams.get("filter")
+
+   const {data: tareas, error} = await supabase.from("tasks").select("*")
 
    let tareasFiltradas = tareas
 
@@ -26,32 +34,33 @@ export async function GET(request){
 export async function POST(request){
 
     const body = await request.json()
+    const title = body.title
+    const completed = false
 
-    const newTask = {
-        id: tareas.length + 1,
-        title: body.title,
-        completed: false
-    }
-    tareas.push(newTask)
-    return new Response(JSON.stringify(newTask), {status: 201})
+    //El .single() es para indicar que es un unico elemento, y no un array. Si no agregaria un array. El single elimina los []
+    //Si la BD devuelve okey, se mete en insertTarea, si no, se mete en error
+    const {data: insertTarea, error} = await supabase.from("tasks").insert({title, completed}).single()
+
+    return new Response(JSON.stringify(insertTarea), {status: 201})
 }
 
 export async function PUT(request){
 
     const body = await request.json()
+    const id = body.id
+    const completed = {completed: !body.completed}
 
-   let tareaAct = tareas.find(tarea => tarea.id === body.id)
+    const {data: updateTarea, error} = await supabase.from("tasks").update(completed).eq("id", id).single()
 
-   tareaAct.completed = !tareaAct.completed
-
-    return new Response(JSON.stringify(tareaAct), {status: 200})
+    return new Response(JSON.stringify(updateTarea), {status: 200})
 }
 
 export async function DELETE(request){
 
     const body = await request.json()
+    const id = body.id
 
-    tareas = tareas.filter(tarea => tarea.id!== body.id)
+    const {data: deleteTask, error} = await supabase.from("tasks").delete().eq("id", id)
 
     return new Response(JSON.stringify({success: "eliminado correctamente"}), {status: 200})
 }
--- src/app/tasks/page.js
+++ src/app/tasks/page.js
@@ -1,75 +1,87 @@
-"use client"
-import Link from 'next/link'
-import {useState, useEffect} from 'react'
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
 
-export default function Tasks(){
+export default function Tasks() {
+  const [tareas, setTareas] = useState([]);
+  const [nuevoTitulo, setNuevoTitulo] = useState("");
+  const [filter, setFilter] = useState("all");
 
-   const [tareas, setTareas] = useState([]) 
-   const [nuevoTitulo, setNuevoTitulo] = useState("")
-   const [filter, setFilter] = useState("all")
+  useEffect(() => {
+    fetchTareas();
+  }, [filter]);
 
+  async function fetchTareas() {
+    let url = "/api/tasks";
 
-   useEffect( () => {
-    fetchTareas()
-   }, [filter])
-
-   async function fetchTareas(){
-    
-    let url = "/api/tasks"
-
-    if(filter !== "all"){
-        url = url + "?filter=" + filter
+    if (filter !== "all") {
+      url = url + "?filter=" + filter;
     }
 
-    const response = await fetch(url)
-    const data = await response.json()
-    setTareas(data)
-   }
+    const response = await fetch(url);
+    const data = await response.json();
+    setTareas(data);
+  }
 
-   async function agregaTarea(e){
+  async function agregaTarea(e) {
     e.preventDefault();
     const response = await fetch("/api/tasks", {
-        method: "POST",
-        headers: {"Content-Type": "application-json"},
-        body: JSON.stringify({title: nuevoTitulo}) 
-    })
-        setNuevoTitulo("")
-        fetchTareas();
-   }
+      method: "POST",
+      headers: { "Content-Type": "application-json" },
+      body: JSON.stringify({ title: nuevoTitulo }),
+    });
+    setNuevoTitulo("");
+    fetchTareas();
+  }
 
-   //{"id": 1}
-   async function completaTarea(idActualizar){
+  //{"id": 1}
+  async function completaTarea(idActualizar, completedAct) {
     const response = await fetch("api/tasks", {
-        method: "PUT",
-        headers: {"Content-Type": "application-json"},
-        body: JSON.stringify({id: idActualizar})
-    })
-
-    fetchTareas()
-   }
-
-   async function eliminaTarea(idEliminar){
-    const response = await fetch("/api/tasks", {
-        method: "DELETE",
-        headers: {"Content-Type": "application-json"},
-        body: JSON.stringify({id: idEliminar})
-    })
+      method: "PUT",
+      headers: { "Content-Type": "application-json" },
+      body: JSON.stringify({ id: idActualizar, completed: completedAct }),
+    });
 
     fetchTareas();
-   }
+  }
 
-    return (
-        <div>
-            <h3>Agregar tarea: </h3>
-            <form onSubmit={agregaTarea} >
-                <input type="text" value={nuevoTitulo} onChange={(e) => setNuevoTitulo(e.target.value)} />
-                <input type="submit" value="Crear" />
-            </form>
-            <h3>Listado de tareas:</h3>
-            <button onClick={() => setFilter("all")}>Todas</button>
-            <button onClick={() => setFilter("completed")}>Completadas</button>
-            <button onClick={() => setFilter("no-completed")}>No completadas</button>
-            {tareas.map(tarea => <p key={tarea.id}><input type="checkbox" checked={tarea.completed} onChange={() => completaTarea(tarea.id)} />{tarea.completed?<s>{tarea.title}</s>:tarea.title}<button onClick={() => eliminaTarea(tarea.id)}>Eliminar</button><Link href={"/tasks/" + tarea.id}>Ver</Link></p>)}
-        </div>
-    );
+  async function eliminaTarea(idEliminar) {
+    const response = await fetch("/api/tasks", {
+      method: "DELETE",
+      headers: { "Content-Type": "application-json" },
+      body: JSON.stringify({ id: idEliminar }),
+    });
+
+    fetchTareas();
+  }
+
+  return (
+    <div>
+      <h3>Agregar tarea: </h3>
+      <form onSubmit={agregaTarea}>
+        <input
+          type="text"
+          value={nuevoTitulo}
+          onChange={(e) => setNuevoTitulo(e.target.value)}
+        />
+        <input type="submit" value="Crear" />
+      </form>
+      <h3>Listado de tareas:</h3>
+      <button onClick={() => setFilter("all")}>Todas</button>
+      <button onClick={() => setFilter("completed")}>Completadas</button>
+      <button onClick={() => setFilter("no-completed")}>No completadas</button>
+      {tareas.map((tarea) => (
+        <p key={tarea.id}>
+          <input
+            type="checkbox"
+            checked={tarea.completed}
+            onChange={() => completaTarea(tarea.id, tarea.completed)}
+          />
+          {tarea.completed ? <s>{tarea.title}</s> : tarea.title}
+          <button onClick={() => eliminaTarea(tarea.id)}>Eliminar</button>
+          <Link href={"/tasks/" + tarea.id}>Ver</Link>
+        </p>
+      ))}
+    </div>
+  );
 }
